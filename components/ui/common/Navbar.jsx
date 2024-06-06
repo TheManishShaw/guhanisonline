1ee6fd6230d68899f115c461813c4d673e3b4286
Manish Shaw <manishshaw725@gmail.com>
2024-06-06 17:26:56 +0530

    fix the navbar close issue in the mobile deveices

--- components/ui/common/Navbar.jsx
+++ components/ui/common/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { CircleUser, Menu, Package2, Search, ShoppingCart } from "lucide-react";
+import { CircleUser, Menu, ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import {
@@ -20,125 +20,123 @@ import { useSession } from "next-auth/react";
 
 const Navbar = () => {
   const path = usePathname();
-  const [cartItemLength, setCartItemLength] = useState();
+  const [cartItemLength, setCartItemLength] = useState(0);
   const user = false;
   const [open, setOpen] = useState(false);
   const cartItems = useSelector((state) => state.cart.items);
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
+
   useEffect(() => {
     setCartItemLength(cartItems.length);
   }, [cartItems]);
+
   useEffect(() => {
-    [SheetClose, { asChild: path === "/beats" }];
+    setOpen(false);
   }, [path]);
+
   return (
     <>
-      {
-        <header className="sticky top-0 flex z-[99] overflow-hidden w-full  h-20 items-center gap-4 border-b bg-background text-white backdrop-blur-lg px-4 md:px-6">
-          <Link className="text-2xl" href="/">
-            <Image
-              src="/assets/images/logo/logo_white.png"
-              width={50}
-              height={50}
-              alt="logo"
-            />
-          </Link>
-          <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-            <form className="ml-auto flex-1 sm:flex-initial">
-              <div className="relative">
-                <nav className="hidden flex-col gap-6 text-2xl font-medium md:flex md:flex-row md:items-center md:gap-5  lg:gap-6">
-                  {menuList.map((menu, index) => (
-                    <Link
-                      key={index}
-                      href={menu.path}
-                      target={menu.target}
-                      className={` ${
-                        path === menu.path
-                          ? "text-primary"
-                          : "text-muted-foreground"
-                      }  transition-colors hover:text-foreground`}
-                    >
-                      {menu.name}
-                    </Link>
-                  ))}
-                </nav>
-              </div>
-            </form>
-            <Link href="/cart">
-              <div className="relative me-2">
-                <ShoppingCart />
-                {cartItemLength?.length != 0 ? (
-                  <p className="-top-2 start-4 absolute w-full text-center bg-white border-2 text-black font-bold border-white  rounded-full">
-                    {cartItemLength}
-                  </p>
-                ) : (
-                  ""
-                )}
-              </div>
-            </Link>
-
-            <Sheet SheetClose={true} open={open} onOpenChange={setOpen}>
-              <SheetTrigger asChild>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="shrink-0 md:hidden"
-                >
-                  <Menu className="h-5 w-5 text-black" />
-                  <span className="sr-only">Toggle navigation menu</span>
-                </Button>
-              </SheetTrigger>
-              <SheetContent
-                side="left"
-                className="bg-background mt-20 text-white"
-              >
-                <nav className="grid gap-6  font-medium">
-                  {menuList.map((menu, index) => (
-                    <Link
-                      key={index + "mobile"}
-                      href={menu.path}
-                      className="text-white text-xl hover:text-foreground"
-                    >
-                      {menu.name}
-                    </Link>
-                  ))}
-                </nav>
-              </SheetContent>
-            </Sheet>
-            {user ? (
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    variant="secondary"
-                    size="icon"
-                    className="rounded-full"
+      <header className="sticky top-0 flex z-[99] overflow-hidden w-full h-20 items-center gap-4 border-b bg-background text-white backdrop-blur-lg px-4 md:px-6">
+        <Link className="text-2xl" href="/">
+          <Image
+            src="/assets/images/logo/logo_white.png"
+            width={50}
+            height={50}
+            alt="logo"
+          />
+        </Link>
+        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+          <form className="ml-auto flex-1 sm:flex-initial">
+            <div className="relative">
+              <nav className="hidden flex-col gap-6 text-2xl font-medium md:flex md:flex-row md:items-center md:gap-5 lg:gap-6">
+                {menuList.map((menu, index) => (
+                  <Link
+                    key={index}
+                    href={menu.path}
+                    target={menu.target}
+                    className={`${
+                      path === menu.path
+                        ? "text-primary"
+                        : "text-muted-foreground"
+                    } transition-colors hover:text-foreground`}
                   >
-                    <CircleUser className="h-5 w-5" />
-                    <span className="sr-only">Toggle user menu</span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  {/* <DropdownMenuItem>Settings</DropdownMenuItem>
-                  <DropdownMenuItem>Support</DropdownMenuItem> */}
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>Logout</DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            ) : session?.user ? (
-              `${session.user.name}`
-            ) : (
-              <Link
-                href={"/sign-in"}
-                className="bg-primary text-black text-2xl px-4 py-1 rounded-md"
+                    {menu.name}
+                  </Link>
+                ))}
+              </nav>
+            </div>
+          </form>
+          <Link href="/cart">
+            <div className="relative me-2">
+              <ShoppingCart />
+              {cartItemLength > 0 ? (
+                <p className="-top-2 start-4 absolute w-full text-center bg-white border-2 text-black font-bold border-white rounded-full">
+                  {cartItemLength}
+                </p>
+              ) : (
+                ""
+              )}
+            </div>
+          </Link>
+
+          <Sheet open={open} onOpenChange={setOpen}>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
               >
-                Login
-              </Link>
-            )}
-          </div>
-        </header>
-      }
+                <Menu className="h-5 w-5 text-black" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent
+              side="left"
+              className="bg-background mt-20 text-white"
+            >
+              <nav className="grid gap-6 font-medium">
+                {menuList.map((menu, index) => (
+                  <Link
+                    key={index + "mobile"}
+                    href={menu.path}
+                    className="text-white text-xl hover:text-foreground"
+                  >
+                    {menu.name}
+                  </Link>
+                ))}
+              </nav>
+            </SheetContent>
+          </Sheet>
+          {user ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  className="rounded-full"
+                >
+                  <CircleUser className="h-5 w-5" />
+                  <span className="sr-only">Toggle user menu</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : session?.user ? (
+            `${session.user.name}`
+          ) : (
+            <Link
+              href={"/sign-in"}
+              className="bg-primary text-black text-2xl px-4 py-1 rounded-md"
+            >
+              Login
+            </Link>
+          )}
+        </div>
+      </header>
     </>
   );
 };
